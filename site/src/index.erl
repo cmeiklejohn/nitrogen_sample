e2aa3eef7f082245ce3d4a5771744be7df525005
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-01-04 21:01:26 -0500

    Simplify.

--- site/src/index.erl
+++ site/src/index.erl
@@ -5,31 +5,9 @@
 
 main() -> #template { file="./site/templates/bare.html" }.
 
-title() -> "Welcome to Nitrogen".
+title() -> "Nitrogen Sample Application".
 
 body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
-
-inner_body() -> 
     [
-        #h1 { text="Welcome to Nitrogen, Bitch" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-        #p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        "
+        #h1 { text="Nitrogen Sample Application" }
     ].
-	
-event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
-    }).
