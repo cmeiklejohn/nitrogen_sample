b18d43c06d846f41767e1de843190dfa2f16623c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-01-04 22:13:54 -0500

    Starter chat application.

--- site/src/index.erl
+++ site/src/index.erl
@@ -5,9 +5,25 @@
 
 main() -> #template { file="./site/templates/bare.html" }.
 
-title() -> "Nitrogen Sample Application".
+title() -> "Chat".
 
-body() ->
-    [
-        #h1 { text="Nitrogen Sample Application" }
-    ].
+body() -> wf:comet_global(fun() -> repeater() end, repeater_pool),
+          [
+            #panel { class="twelve columns offset-by-one content", body = [
+              #h1 { text="Chat" },
+              #label { text="Your message:" },
+              #textbox { id=msg, text="", next=submit },
+              #button { id=submit, text="Say", postback=submit },
+              #panel { id=placeholder }
+            ]}
+          ].
+
+event(submit) ->
+  wf:send_global(repeater_pool, {msg, wf:q(msg)}).
+
+repeater() ->
+  receive 
+    {msg, Msg} -> wf:insert_top(placeholder, [Msg, "<br>"])
+  end,
+  wf:flush(),
+  repeater().
